7167cbe206d8cbecb193c8411b5344c5b1f6a9bc
Алексей Демиденко <kuberg8@gmail.com>
2023-08-01 14:19:54 +0500

    e9e2eb1

---
+++ _nuxt/static/1690881588/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ffigaro\u002F_nuxt\u002Fstatic\u002F1690881588",layout:"default",error:a,state:{cards:[],cardsTitle:""},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ffigaro\u002F",assetsPath:"\u002Ffigaro\u002F_nuxt\u002F",cdnURL:a}}}}(null));
